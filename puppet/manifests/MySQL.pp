b6849cb577558392adad3daabbe70750a36f3826
Jeffrey Lee <jeffrey.lee@ptminconline.com>
2014-08-08 20:10:51 +0000

    This is a commit of the correct MySQL.pp with root pass config

--- puppet/manifests/MySQL.pp
+++ puppet/manifests/MySQL.pp
@@ -1,9 +1,23 @@
-package {'mysql-server':
-  ensure => 'present',
-}
-
-service {'mysqld':
-  ensure => running,
-  enable => true,
-  require => Package['mysql-server'],
+class mysql {
+  package { "mysql-server":
+    ensure => present,
+  }
+  service { "mysql":
+    ensure => running,
+    enable => true,
+    hasstatus => true,
+    require => Package["mysql-server"], 
+  }
+  file { "/etc/mysql/my.cnf":
+    ensure => present,
+    content => template("mysql/my.cnf.erb"),
+    notify => Service["mysql"],
+    require => Package["mysql-server"],
+  }
+  exec { "set mysql root password":
+    path => "/usr/bin",
+    unless => "mysql -uroot -p$ecretpa$$",
+    command => "mysqladmin -u root password $ecretpa$$",
+    require => Service['mysql'],
+  }
 }
