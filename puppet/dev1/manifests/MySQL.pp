01746ff4d6b242435315bd32a8678c88a637ba17
Jeff Lee <jeffrey.lee@ptminconline.com>
2014-08-20 17:15:07 -0400

    removed mysql bindings

--- puppet/dev1/manifests/MySQL.pp
+++ puppet/dev1/manifests/MySQL.pp
@@ -1,9 +1,5 @@
 include mysql::server
 
-class { 'mysql::bindings':
-    php_enable => true,
-  }
-
   exec { "set-mysql-password":
     unless => "mysqladmin uroot -psecretpwd status",
     path => "/usr/bin",
