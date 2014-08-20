1257496d180ad36b696d82e1a23edf8398563239
Jeff Lee <jeffrey.lee@ptminconline.com>
2014-08-20 16:14:51 -0400

    Modified MySQL.pp

--- puppet/dev1/manifests/MySQL.pp
+++ puppet/dev1/manifests/MySQL.pp
@@ -1,19 +1,25 @@
 include mysql::server
 
-class mysql {
+class { 'mysql::bindings':
+    php_enable => true,
+  }
+
   package { "mysql-server":
     ensure => present
   }
+
   service { "mysqld":
     ensure => running,
     enable => true,
     hasstatus => true,
     require => Package["mysql-server"],
   }
+
   exec { "set mysql root password":
     path => "/usr/bin",
     unless => "mysql -uroot -p$ecretpa$$",
     command => "mysqladmin -u root password $ecretpa$$",
     require => Service['mysql'],
   }
-}
+
+
