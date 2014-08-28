bb4fba3c07f9efcae5c70c69c25a8714677496cb
Jeff Lee <jeffrey.lee@ptminconline.com>
2014-08-28 16:26:54 -0400

    adding apache php 5

--- puppet/dev1/manifests/ApachePHP5.pp
+++ puppet/dev1/manifests/ApachePHP5.pp
@@ -2,26 +2,9 @@ class {"apache":
 	mpm_module => 'prefork'
 }
  
-class {"apache::mod::php":
-	require => Exec["update"]
-}
-class { 'php': 
-	require => Exec["update"]
-}
-
-apache::mod { 'rewrite': }
- 
-php::module { "mcrypt": }
-php::module { "curl": }
-php::module { "gd": }
-php::module { "xdebug": }
-php::module { "mysql": }
-
 apache::vhost{"localhost":
 	port	=> 80,
-	docroot	=>	"/var/www/html",
+	docroot	=>"/var/www/html",
 }
-
-include php
 include apache
 include apache::mod::php
