bd2f89cff219addfc143e02e23bf0372656c5d8e
Jeff Lee <jeffrey.lee@ptminconline.com>
2014-08-28 15:21:54 -0400

    updated the apachephp5.pp

--- puppet/dev1/manifests/ApachePHP5.pp
+++ puppet/dev1/manifests/ApachePHP5.pp
@@ -1,10 +1,27 @@
-class { 'apache':
-    mpm_module => 'prefork',
+#puppet module install example42/php;
+#puppet module install puppetlabs/apache;
+#puppet module install puppetlabs/mysql"
+
+class {"apache": 
+	mpm_module => 'prefork'
+}
+ 
+class {"apache::mod::php":
+	require => Exec["update"]
+}
+class { 'php': 
+	require => Exec["update"]
 }
 
-include apache::mod::php
+apache::mod { 'rewrite': }
+ 
+php::module { "mcrypt": }
+php::module { "curl": }
+php::module { "gd": }
+php::module { "xdebug": }
+php::module { "mysql": }
 
-apache::vhost { 'localhost':
-    port => "80",
-    docroot => "/var/www/html" 
+apache::vhost{"localhost":
+	port	=> 80,
+	docroot	=>	"/var/www/html",
 }
