7e74684b75e490a915a2438764e6a3413cd6160e
Jeff Lee <jeffrey.lee@ptminconline.com>
2014-09-05 14:55:42 -0400

    trying new apache php manifest

--- puppet/dev1/manifests/ApachePHP5.pp
+++ puppet/dev1/manifests/ApachePHP5.pp
@@ -1,10 +1,23 @@
-class {"apache": 
-	mpm_module => 'prefork'
+# install apache2 package
+package { 'apache2':
+  require => Exec['apt-update'],        # require 'apt-update' before installing
+  ensure => installed,
 }
- 
-apache::vhost{"localhost":
-	port	=> 80,
-	docroot	=>"/var/www/html",
+
+# ensure apache2 service is running
+service { 'apache2':
+  ensure => running,
 }
-include apache
-include apache::mod::php
+
+# install php5 package
+package { 'php5':
+  require => Exec['apt-update'],        # require 'apt-update' before installing
+  ensure => installed,
+}
+
+# ensure info.php file exists
+file { '/var/www/html/info.php':
+  ensure => file,
+  content => '<?php  phpinfo(); ?>',    # phpinfo code
+  require => Package['apache2'],        # require 'apache2' package before creating
+} 
